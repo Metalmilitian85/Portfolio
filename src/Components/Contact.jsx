117ca315dd5687dc16393bfb44734278a121f58d
Tim <timward1985@gmail.com>
2023-06-09 15:44:34 -0500

    Contacts

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import ButtonMailto from './Email'
 
 function Contact() {
   return (
-    <div className='text-[#4bb3f8] p-20 text-center'>
+    <div className='text-[#4bb3f8] p-20 text-center font-semibold'>
         <h3 className='font-bold text-3xl'>Contact</h3>
-          <div className='flex justify-center space-x-4'>
-            <img className='h-6 animate-spin -z-10' src="./emailicon.png" />
-            <p> timward1985@gmail.com</p>
+          <div className='flex justify-center space-x-3 p-2'>
+            <img className='h-6 -z-10 rounded-full' src="./emailicon.png" />
+            <button onClick={() => window.location = 'mailto:timward1985@gmail.com'}>timward1985@gmail.com</button>
           </div>
-          <p className='animate-type'>Linked-In</p>
-          <p>Phone</p>
+          <div className='flex justify-center p-2 space-x-3'>
+            <img className='h-6 -z-10' src="./linkedin.png" />
+            <a href="https://www.linkedin.com/in/tim-ward-827b111ab/">Linked In</a>
+          </div>
+          <div className='flex justify-center p-2 space-x-3'>
+            <img className='h-6 -z-10 rounded-full' src="./phone.jpg" />
+            <a href="tel:9132310183">913-231-0183</a>
+          </div>
     </div>
   )
 }
