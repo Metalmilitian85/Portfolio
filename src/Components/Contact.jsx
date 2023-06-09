384f0af50d688c522a151130116df9da7e27dc49
Tim <timward1985@gmail.com>
2023-06-09 11:53:15 -0500

    zindex

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <div className='text-[#4bb3f8] p-20 text-center'>
         <h3 className='font-bold text-3xl'>Contact</h3>
           <div className='flex justify-center space-x-4'>
-            <img className='h-6 animate-spin' src="./emailicon.png" />
+            <img className='h-6 animate-spin -z-10' src="./emailicon.png" />
             <p> timward1985@gmail.com</p>
           </div>
           <p className='animate-type'>Linked-In</p>
