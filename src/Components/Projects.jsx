10df9e5a69d71edefcfe81739f7e7fa266e74c42
Tim <timward1985@gmail.com>
2023-06-20 16:07:08 -0500

    weatherapp

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,6 +5,8 @@ function Projects() {
     <div className="text-center">
         <p className='p-10 text-[#4bb3f8] font-bold'>Projects</p>
         <a href="https://incomparable-sherbet-60d556.netlify.app/" target="_blank" className="project" >Pet Adoption</a>
+        <br></br>
+        <a href="https://marvelous-dragon-48b7bb.netlify.app" target="_blank" className="weather" >Weather App</a>
     </div>
   )
 }
