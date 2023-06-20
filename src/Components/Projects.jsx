74835135471a202ff9bc31510484c9db5870b02b
Tim <timward1985@gmail.com>
2023-06-20 11:11:00 -0500

    pet link

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>
-        <p className='p-10 text-[#4bb3f8] text-center font-bold'>Projects</p>
+    <div className="text-center">
+        <p className='p-10 text-[#4bb3f8] font-bold'>Projects</p>
+        <a href="https://incomparable-sherbet-60d556.netlify.app/" className="text-[#4bb3f8] font-bold">Pets Project</a>
     </div>
   )
 }
