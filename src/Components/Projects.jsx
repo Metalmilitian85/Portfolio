15454ed0f70ece5b3468bae1f30cf006772ef9f9
Tim <timward1985@gmail.com>
2023-07-10 08:02:02 -0500

    projectsquares

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,11 +4,19 @@ function Projects() {
   return (
     <div className="text-center">
         <p className='p-10 text-[#4bb3f8] font-bold'>Projects</p>
-        <a href="https://incomparable-sherbet-60d556.netlify.app/" target="_blank" className="project" >Pet Adoption</a>
+        <div>
+          <a href="https://incomparable-sherbet-60d556.netlify.app/" target="_blank" className="project" >Pet Adoption</a>
+          <img src="adoptions.jpg" className="border-4 rounded-xl border-purple-500 mx-auto max-w-[50%]" />
+        </div>
+        {/*<br></br>
+        <div>
+          <a href="https://marvelous-dragon-48b7bb.netlify.app" target="_blank" className="weather" >Weather App</a>
+        </div>*/}
         <br></br>
-        <a href="https://marvelous-dragon-48b7bb.netlify.app" target="_blank" className="weather" >Weather App</a>
-        <br></br>
-        <a href="https://prismatic-pasca-506ef1.netlify.app/" target="_blank" className="scrolls" >The Elder Scrolls</a>
+        <div>
+          <a href="https://prismatic-pasca-506ef1.netlify.app/" target="_blank" className="scrolls" >The Elder Scrolls</a>
+          <img src="skyrimbg.webp" className="mx-auto max-w-[50%] border-4 rounded-xl border-gray-500" />
+        </div>
     </div>
   )
 }
