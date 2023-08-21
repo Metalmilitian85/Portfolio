807a05705e8cbaac330cee724cac4ea3374d5de9
Tim <timward1985@gmail.com>
2023-08-21 11:34:54 -0500

    targetlinks

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,11 +7,11 @@ function Contact() {
         <h1 className="w-full text-2xl font-bold text-center text-[#4bb3f8] hover:scale-105 duration-300 pt-5 mb-7">CONTACT</h1>
           <div className='flex justify-center p-2 space-x-3'>
             <img className='h-6' src="./linkedin.png" />
-            <a href="https://www.linkedin.com/in/tim-ward-827b111ab/">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/tim-ward-827b111ab/" target="_blank" >LinkedIn</a>
           </div>
           <div className='flex justify-center p-2 space-x-3'>
             <img className='h-6 rounded-full' src="./github.png" />
-            <a href="https://github.com/Metalmilitian85?tab=repositories">GitHub</a>
+            <a href="https://github.com/Metalmilitian85?tab=repositories" target="_blank" >GitHub</a>
           </div>
           <div className='flex justify-center space-x-3 p-2'>
             <img className='h-6 rounded-full' src="./emailicon.png" />
