7d788e7418ec536f19414270c00b83bfaeb3436a
Tim <timward1985@gmail.com>
2023-08-29 15:35:41 -0500

    cursor

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,17 +26,16 @@ const Navbar = () => {
 
   return (
     <div className="fixed flex justify-between items-center h-20 w-[100%] mx-auto px-4 bg-black text-sky-500 font-bold z-10">
-        <Link to='About' spy={true} smooth={true} offset={-80} duration={500}><h1 className="w-full text-3xl font-bold text-sky-500 hover:animate-pulse">TIM WARD</h1></Link>
+        <Link to='About' spy={true} smooth={true} offset={-80} duration={500}><h1 className="w-full text-3xl font-bold text-sky-500 hover:animate-pulse hover:cursor-pointer">TIM WARD</h1></Link>
         <div className="hidden md:flex justify-between mr-auto mx-7">
           <a href="https://www.linkedin.com/in/tim-ward-827b111ab/" target="_blank" ><img src="linkedin.png" className="h-6 hover:cursor-pointer mr-7 hover:scale-110 duration-300" /></a>
           <a href="https://github.com/Metalmilitian85?tab=repositories" target="_blank" ><img src="github.png" className="h-6 hover:cursor-pointer rounded-full mr-7 hover:scale-110 duration-300" /></a>
-          <button onClick={() => window.location = 'mailto:timward1985@gmail.com'}><img src="emailicon.png" className="h-6 rounded-full mr-6 hover:cursor-pointer hover:scale-110 duration-300" /></button>
         </div>
         <ul className="hidden md:flex text-blue">
-            <Link to='About' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300">ABOUT</li></Link>
-            <Link to='Projects' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300">PROJECTS</li></Link>
-            <Link to='Skills' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300">SKILLS</li></Link>
-            <Link to='Contact' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300">CONTACT</li></Link>
+            <Link to='About' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300 hover:cursor-pointer">ABOUT</li></Link>
+            <Link to='Projects' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300 hover:cursor-pointer">PROJECTS</li></Link>
+            <Link to='Skills' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300 hover:cursor-pointer">SKILLS</li></Link>
+            <Link to='Contact' spy={true} smooth={true} offset={-80} duration={500}><li className="p-4 hover:text-[#02daf7] hover:scale-105 duration-300 hover:cursor-pointer">CONTACT</li></Link>
         </ul>
         <div onClick={handleNav} className="block md:hidden">
             {!nav ? <AiOutlineClose size={20} color="[#4bb3f8]" /> : <AiOutlineMenu size={20} color="[#4bb3f8]" />}
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
         <h1 className="w-full text-2xl font-bold text-center text-[#4bb3f8] hover:scale-105 duration-300 mb-10">PROJECTS</h1>
         <div>
           <a href="https://briansburgers.netlify.app/" target="_blank" className="burgers" >Brian's Burger Bash
-            <img src="burgerbash.jpg" className="mx-auto max-w-[40%] min-h-[130px] max-h-[300px] border-4 rounded-xl border-red-600 mt-4 mb-20 hover:scale-105 duration-300 " /></a>
+            <img src="burgerbash.jpg" className="mx-auto max-w-[40%] min-h-[130px] max-h-[300px] border-4 rounded-xl border-red-600 mt-4 mb-20 hover:scale-105 duration-300" /></a>
           <a href="https://flixpix.netlify.app/" target="_blank" className="flixpix" >FLIXPIX
             <img src="flixpix.jpg" className="mx-auto max-w-[40%] min-h-[130px] max-h-[300px] border-4 rounded-xl border-sky-500 mt-4 mb-10 hover:scale-105 duration-300 " /></a> 
         </div>
