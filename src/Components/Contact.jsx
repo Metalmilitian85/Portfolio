4d33a9baadcdd045a809754e1e14e60dec94308b
Tim <timward1985@gmail.com>
2023-08-29 15:29:06 -0500

    contactbox

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import ButtonMailto from './Email'
+import { Email } from './Email'
 
 function Contact() {
   return (
-    <div id="Contact" className='text-[#4bb3f8] p-12 text-center font-semibold md:text-lg'>
-        <h1 className="w-full text-2xl font-bold text-center text-[#4bb3f8] hover:scale-105 duration-300 pt-5 mb-7">CONTACT</h1>
-          <div className='flex justify-center p-2 space-x-3'>
-            <img className='h-6' src="./linkedin.png" />
-            <a href="https://www.linkedin.com/in/tim-ward-827b111ab/" target="_blank" >LinkedIn</a>
-          </div>
-          <div className='flex justify-center p-2 space-x-3'>
-            <img className='h-6 rounded-full' src="./github.png" />
-            <a href="https://github.com/Metalmilitian85?tab=repositories" target="_blank" >GitHub</a>
-          </div>
-          <div className='flex justify-center space-x-3 p-2'>
-            <img className='h-6 rounded-full' src="./emailicon.png" />
-            <button onClick={() => window.location = 'mailto:timward1985@gmail.com'}>timward1985@gmail.com</button>
-          </div>
-          <div className='flex justify-center p-2 space-x-3'>
-            <img className='h-6 rounded-full' src="./phone.jpg" />
-            <a href="tel:9132310183">913-231-0183</a>
-          </div>
+    <div>
+      <h1 className="w-full text-2xl font-bold text-center text-[#4bb3f8] hover:scale-105 duration-300 pt-16 mb-7">CONTACT</h1>
+      <Email />
+      <div id="Contact" className='text-[#4bb3f8] p-12 text-center font-semibold md:text-lg'>
+            <div className='flex justify-center p-2 space-x-3'>
+              <img className='h-6' src="./linkedin.png" />
+              <a href="https://www.linkedin.com/in/tim-ward-827b111ab/" target="_blank" >LinkedIn</a>
+            </div>
+            <div className='flex justify-center p-2 space-x-3'>
+              <img className='h-6 rounded-full' src="./github.png" />
+              <a href="https://github.com/Metalmilitian85?tab=repositories" target="_blank" >GitHub</a>
+            </div>
+            <div className='flex justify-center space-x-3 p-2'>
+              <img className='h-6 rounded-full' src="./emailicon.png" />
+              <button onClick={() => window.location = 'mailto:timward1985@gmail.com'}>timward1985@gmail.com</button>
+            </div>
+            <div className='flex justify-center p-2 space-x-3'>
+              <img className='h-6 rounded-full' src="./phone.jpg" />
+              <a href="tel:9132310183">913-231-0183</a>
+            </div>
+      </div>
     </div>
   )
 }
--- src/Components/Email.jsx
+++ src/Components/Email.jsx
@@ -1,18 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-const ButtonMailto = ({ mailto, label }) => {
-    return (
-        <Link
-            to='#'
-            onClick={(e) => {
-                window.location.href = mailto;
-                e.preventDefault();
-            }}
-        >
-            {label}
-        </Link>
-    );
+export const Email = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_zjvk4ba', 'template_leg03y6', form.current, 'B8EBsqUwNAtvSE1zw')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  console.log(sendEmail);
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="flex flex-col mx-7 md:w-[30%] md:mx-auto">
+      <input 
+        type="text" 
+        name="user_name" 
+        className="rounded-md border-2 border-[#4bb3f8] mb-5"
+        placeholder='Name'
+      />
+      <input 
+        type="email" 
+        name="user_email" 
+        className="rounded-md border-2 border-[#4bb3f8] mb-5"
+        placeholder='Email'
+      />
+      <textarea 
+        name="message" 
+        className='h-[100px] rounded-md border-2 border-[#4bb3f8]'
+        placeholder='Send me a message!'
+      />
+      <input type="submit" value="Send" className="font-semibold w-16 mx-auto mt-7 rounded-md bg-sky-300 hover:cursor-pointer hover:border-sky-800 hover:bg-sky-500 border-2 border-sky-600" />
+    </form>
+  );
 };
-
-export default ButtonMailto;
